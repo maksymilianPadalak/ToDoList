7a1a0a01239c0e0136cc8e5ef2154c643074c9a9
Maksymilian Padalak <maks.padalak@netcorp.ee>
2022-10-08 11:49:24 +0200

    Add bootstrap to project

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
