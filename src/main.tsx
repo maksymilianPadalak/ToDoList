ec36acefae099dd88d7dd0ebdf76c7d79118eabd
Maksymilian Padalak <maks.padalak@netcorp.ee>
2022-10-08 12:38:28 +0200

    Create custom bootstrap configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import "bootstrap/dist/css/bootstrap.css";
+import "./custom.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
